baa64a58f611001e42299821f3796e621c827f14
Zvone <zvonimircicvaric@windowslive.com>
2019-12-01 17:03:49 +0100

    Vjezba 4

---
+++ Vjezba 4/src/main/java/Vjezba4/Sensor.java
@@ -0,0 +1,27 @@
+package Vjezba4;
+
+
+
+public final class Sensor {
+	private short value;
+	private short factor;
+	private double minValue;
+	private double maxValue;
+	private String unitOfMeasure;
+	
+        Sensor(int factor, double minValue,double maxValue,String unitOfMeasure){ 
+		this.value = generateRandomValue();
+		this.factor = (short)factor;
+		this.minValue = minValue;
+		this.maxValue = maxValue;
+		this.unitOfMeasure = unitOfMeasure;
+	}
+	short generateRandomValue() {
+		return (short) ( this.minValue + Math.random() * (this.maxValue-this.minValue));
+	}
+        
+        String getUnitOfMeasure() {
+            return this.unitOfMeasure;
+        }
+
+}
